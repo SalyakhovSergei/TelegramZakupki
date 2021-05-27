0d02f46fe12cf9fbfb6b4b51eae257500ae514eb
Саляхов Сергей <salyakhovsergei@gmail.com>
2021-05-27 22:32:17 +1000

    1

--- telegramnew/BotCredentials.cs
+++ telegramnew/BotCredentials.cs
@@ -8,7 +8,7 @@ namespace telegramnew
 {
     public class BotCredentials
     {
-        public static readonly string token = "_";
+        public static readonly string token = "1766453983:AAEe5ARsvrFb0uZK0a9B9QeEddIvFZrucvo";
         public static readonly string connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\admin\source\repos\telegramnew\telegramnew\Database1.mdf;Integrated Security=True";
     }
 }

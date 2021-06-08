afbfd61258b75fbb6f447e66013ec3de4d8a9a13
Sergey <ssm1988@inbox.ru>
2021-06-08 16:16:39 +1000

    1

---
+++ telegramnew/OrderInstructions/IBotLogic.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramTestBot.OrderInstructions
+{
+    public interface IBotLogic
+    {
+        public void Initialize();
+        public void Start();
+        public void Stop();
+    }
+}
---
+++ telegramnew/OrderInstructions/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TelegramTestBot.Models;
+
+namespace TelegramTestBot.OrderInstructions
+{
+    public interface IOrderRepository
+    {
+        public void CreateOrder (Order order);
+        
+        public Order[] GetOrdersByUserId (long userId);
+
+        public Order GetOrderById(Guid orderId);
+    }
+}
